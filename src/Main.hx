62a24469d6598d3a8c8d636304f7a5458ba19c81
Ian Harrigan <ianharrigan@hotmail.com>
2019-08-08 14:40:03 +0200

    embedabble

--- src/Main.hx
+++ src/Main.hx
@@ -5,7 +5,18 @@ import views.MainView;
 
 class Main {
     public static function main() {
-        var app = new HaxeUIApp();
+        var options = null;
+        
+        #if js
+            var container = js.Browser.window.document.getElementById("haxeui-container");
+            if (container != null) {
+                options = {
+                    container: container
+                }
+            }
+        #end
+        
+        var app = new HaxeUIApp(options);
         app.ready(function() {
             var main = new MainView();
             app.addComponent(main);
