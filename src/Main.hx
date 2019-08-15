aaf4301ffc554c5cc1440aa383ad7ce1ad4bf4d0
Ian Harrigan <ianharrigan@hotmail.com>
2019-08-15 11:11:36 +0200

    wip

--- src/Main.hx
+++ src/Main.hx
@@ -5,18 +5,7 @@ import views.MainView;
 
 class Main {
     public static function main() {
-        var options = null;
-        
-        #if js
-            var container = js.Browser.window.document.getElementById("haxeui-container");
-            if (container != null) {
-                options = {
-                    container: container
-                }
-            }
-        #end
-        
-        var app = new HaxeUIApp(options);
+        var app = new HaxeUIApp();
         app.ready(function() {
             var main = new MainView();
             app.addComponent(main);
